d9b598c47f7faeed4f2cc09f287c2a17c4f85938
imamadyan <aimamadyan.id@gmail.com>
2021-11-07 15:33:53 +0700

    "Fix for Node Modules in Utils"

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,3 +1,6 @@
+const request = require('request')
+const chalk = require('chalk')
+
 const weather = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=c3d88d8acbaff51e8e2f93bc007d8f04&query=' + latitude + ',' + longitude
 
